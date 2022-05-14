187d0339575b23fd656735816d2352fb247c299c
khatova <khatova@uni-potsdam.de>
2022-05-14 16:20:03 +0200

    Merger divided into several modules

--- run_clingo.py
+++ run_clingo.py
@@ -1,7 +1,9 @@
-import os
+#!/usr/bin/python
 
-def run(plan1, plan2, merger, final_plan):
-    command = "".join(["clingo --out-atomf='%s.' -V0 -c horizon=15  ",plan1," ",plan2," ",merger," > ",final_plan])
+import os, sys, getopt, argparse
+
+def run(input_plans,merger,output_plan,action,conflicts_detector,input_parser,output_format):
+    command = "".join(["clingo --out-atomf='%s.' -V0 -c horizon=15  ",input_plans," ",conflicts_detector," ",merger," ",action," ",input_parser," ",output_format," > ",output_plan])
     print("Command: {}".format(command))
 
     stream = os.popen(command)
@@ -36,12 +38,51 @@ def visualize(plan):
     else:
         print("Command runned with output... : {}".format(output))
 
-if __name__ == "__main__":
-    plan1 = 'plans/original/plan_x3y2r1s2p2_1_original.lp'
-    plan2 = 'plans/original/plan_x3y2r1s2p2_2_original.lp'
-    merger = "merger_traffic.lp"
-    final_plan = 'plans/merged/x2y3r2s2p2_traffic_plan.lp'
+def main(argv):
+    parser = argparse.ArgumentParser(description='Command runs custom plan merger using clingo')
 
-    run(plan1,plan2,merger,final_plan)
-    aesthetic(final_plan)
-    visualize(final_plan)
+    input_plans = 'plans/original/{plan_1_original.lp,plan_2_original.lp}'
+    merger = "merger.lp"
+    output_plan = 'plans/merged/out_plan.lp'
+
+    action = 'action.lp'
+    conflicts_detector = 'conflicts_detector.lp'
+    input_parser = 'asprilo_output_to_input.lp'
+    output_format = 'merger_output.lp'
+
+    help_line = 'run_clingo.py -i <input_plans> -o <output_plans> -m <merger> -a <action> -c <conflicts_detector> -p <input_parser> -f <output_format>'
+
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:m:c:p:f:")
+    except getopt.GetoptError:
+        print(help_line)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print(help_line)
+            sys.exit()
+        elif opt == '-i':
+            input_plans = arg
+        elif opt == "-o":
+            output_plan = arg
+        elif opt == '-m':
+            merger = arg
+        elif opt == '-a':
+            action = arg
+        elif opt == '-c':
+            conflicts_detector = arg
+        elif opt == '-p':
+            input_parser = arg
+        elif opt == '-f':
+            output_format = arg
+
+    run(input_plans,merger,output_plan,action,conflicts_detector,input_parser,output_format)
+    aesthetic(output_plan)
+    visualize(output_plan)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
+    
+
+    
