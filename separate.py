71f8caff7a9fc7551e43f9864cebb074a77fa58c
khatova <khatova@uni-potsdam.de>
2022-09-01 23:25:21 +0200

    separation of plans

--- separate.py
+++ separate.py
@@ -4,7 +4,8 @@ import os
 import re
 import sys
 
-from utils import read_file
+from utils import read_file, empty_folder, delete_instances
+from make_plans import run
 
 help_line = "separate.py -i <instance.lp> -o <out_folder>"
 
@@ -37,27 +38,39 @@ def main(argv):
     if len(instance) == 0:
         print(help_line)
     else:
+        empty_folder(out_folder)
         common_lines = []
         lines = read_file(instance)
-        out_files = []
+        instance_files = []
+        horizon = 0
         for line in lines:
             # init(object(node, 21), value(at, (1, 3))).
+            m = re.search(r'init\(object\(node', line)
+            if m:
+                horizon += 1
+                common_lines.append(line)
             # init(object(shelf,2), value(at,(1,2))).
-            if re.search(r'init\(object\(node', line) or re.search(r'init\(object\(shelf', line):
+            elif re.search(r'init\(object\(shelf', line):
                 common_lines.append(line)
             else:
                 # init(object(robot,1), value(at,(5,10))).
                 m = re.search(r'init\(object\(robot,(\d*)\)', line)
                 if m:
-                    filename = os.path.join(out_folder, "plan_only_{}.lp".format(m.group(1)))
-                    if not filename in out_files:
-                        out_files.append(filename)
+                    filename = os.path.join(out_folder, "instance_{}.lp".format(m.group(1)))
+                    if not filename in instance_files:
+                        instance_files.append(filename)
                     with open(filename, 'a') as file:
                         file.write(line)
 
-            for filename in out_files:
+            for filename in instance_files:
                 with open(filename, 'a') as file:
                     file.writelines(common_lines)
+
+        for instance in instance_files:
+            r = re.find(r'\d+', instance)
+            run(instance,out_folder,f"plan_{r}.lp",horizon)
+
+        delete_instances(out_folder)
 
         print("done.")
 
